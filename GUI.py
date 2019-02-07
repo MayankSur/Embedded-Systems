aa6ef54f42d4caf0323690dc8af30a5bfaf27b8d
Barrometer <pickup.edward@gmail.com>
2019-02-07 11:40:25 +0000

    First fake GUI

--- GUI.py
+++ GUI.py
@@ -7,43 +7,48 @@ class Window(Frame):
 	def __init__(self, master=None):
 		Frame.__init__(self, master)               
 		self.master = master
-		self.init_window()
+		#self.init_window()
 
-	def init_window(self):
+	#def init_window(self):
 		# changing the title of our master widget      
-		self.master.title("GUI")
+		self.master.title("LitterRate Desktop Application")
 
 		# allowing the widget to take the full space of the root window
 		self.pack(fill=BOTH, expand=1)
 
 		# creating a button instance
-		quitButton = Button(self, text="Quit", command=self.client_exit)
+		self.quitButton = Button(self, text="Quit", command=self.client_exit)
 		
 		# creating a button instance
-		getDistDataButton = Button(self, text="Get New Reading", command=self.client_exit)
+		self.getDistDataButton = Button(self, text="Get New Reading", command=self.fakeTextUpdate1)
 		
 		# creating a button instance
-		getLightDataButton = Button(self, text="Get Most Recent Reading", command=self.client_exit)
+		self.getLightDataButton = Button(self, text="Get Most Recent Reading", command=self.fakeTextUpdate2)
 		
 		#creating the text output area
-		outputInfoArea = Label(self, text="test")
-		outputInfoArea.place(x=100,y=200)
-		#outputInfoArea.pack()
+		
+		#self.outText = StringVar()
+		#self.outText.set("Initial Value")
+		self.dataLabel = Label(self, text="Initial Values for Test")
+		self.dataLabel.place(x=100,y=200)
 		
 		# placing the button on my window
-		quitButton.place(x=300, y=250)
+		self.quitButton.place(x=300, y=250)
 		
-		getDistDataButton.place (x = 100, y =100)
-		getLightDataButton.place(x = 250, y =100)
+		self.getDistDataButton.place (x = 0, y =0)
+		self.getLightDataButton.place(x = 250, y =0)
 		
-		w = Label(self, text="Hello, world!")
-		w.pack()
+		#self.w = Label(self, text="Hello, world!")
+		#self.w.pack()
 		
 		
 		
 	def client_exit(self):
 			exit()
-	
+	def fakeTextUpdate1(self):
+			self.dataLabel.config(text="Fake Reading. Time = -1, Sensor = pi, Bin is a lie")
+	def fakeTextUpdate2(self):
+			self.dataLabel.config(text="Fake Reading. Time = e, Sensor = -2, Bin is president")
 		
 root = Tk()
 root.geometry("400x300")
