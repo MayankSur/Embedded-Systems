692fc758f34b12b569e76b865e966d3955198851
root <root@DESKTOP-9M33IJE.localdomain>
2019-02-07 16:53:36 +0000

    Connected the GUI MAD-ONE!

--- mqtt_test_computer.py
+++ mqtt_test_computer.py
@@ -1,10 +1,77 @@
 import paho.mqtt.client as mqtt
 import time
 import json
+from tkinter import *
+
+from tkinter import messagebox
 
 bottom_value = 0
 curr_ultra_value = 0
 curr_light_value = 0
+curr_time = ''
+
+class Window(Frame):
+
+	def __init__(self, master=None):
+		Frame.__init__(self, master)               
+		self.master = master
+		#self.init_window()
+
+	#def init_window(self):
+		# changing the title of our master widget      
+		self.master.title("LitterRate Desktop Application")
+
+		# allowing the widget to take the full space of the root window
+		self.pack(fill=BOTH, expand=1)
+
+		# creating a button instance
+		self.quitButton = Button(self, text="Quit", command=self.client_exit)
+		
+		# creating a button instance
+		self.getDistDataButton = Button(self, text="Get New Reading", command=self.Update1)
+		
+		# creating a button instance
+		self.getLightDataButton = Button(self, text="Calibrate", command=self.calibrate)
+		
+		#creating the text output area
+		self.dataSectionHeader=Label(self, text = "Data Reading")
+		self.dataSectionHeader.place(x=50,y=100)
+		self.timeLabel=Label(self,text="Time: ")
+		self.timeLabel.place(x=50,y=120)
+		self.readingLabel=Label(self,text="Reading: ")
+		self.readingLabel.place(x=50,y=140)
+		self.valueLabel=Label(self,text="Value: ")
+		self.valueLabel.place(x=50,y=160)
+		
+		
+		# placing the button on my window
+		self.quitButton.place(x=300, y=250)
+		
+		self.getDistDataButton.place (x = 0, y =0)
+		self.getLightDataButton.place(x = 250, y =0)
+		
+		#self.w = Label(self, text="Hello, world!")
+		#self.w.pack()
+		
+	
+	def client_exit(self):
+			exit()
+	def Update1(self):
+		global curr_light_value
+		global curr_ultra_value
+		global curr_time
+		message=client.publish("IC.embedded/ALphawolfSquadron/send","get_data")
+		time.sleep(2)
+		print('Changed', curr_ultra_value)
+		print('Changed', curr_light_value)
+		self.timeLabel.config(text="Time: " + curr_time)
+		self.readingLabel.config(text="Reading-Light: " + str(curr_light_value))
+		self.valueLabel.config(text="Reading-Ultra: " + str(curr_ultra_value))
+	def calibrate(self):
+			message=client.publish("IC.embedded/ALphawolfSquadron/send","get_empty")
+			messagebox.showinfo("tk", "RE-CALIBRATION COMPLETE")
+		
+	
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -17,6 +84,9 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+	global curr_light_value
+	global curr_ultra_value
+	global curr_time
 	topic=msg.topic
 	m_decode=str(msg.payload.decode("utf-8","ignore"))
 	#print("data Received type",type(m_decode))
@@ -31,11 +101,11 @@ def on_message(client, userdata, msg):
 	
 	print(m_in)
 	curr_ultra_value = int(m_in['ultrasonic_value'])
-	print('entered')
+	print('Changed', curr_ultra_value)
 	curr_light_value = int(m_in['lightsensor_value'])
-	print('entered')
+	print('Changed', curr_light_value)
 	
-	
+	curr_time = str(m_in['time'])
 	
 
 client = mqtt.Client()
@@ -51,27 +121,32 @@ client.connect("test.mosquitto.org", 1883, 60)
 # manual interface.
 client.loop_start()
 
-while True:
-		time.sleep(4)
-		command = input('Enter a number: ')
+root = Tk()
+root.geometry("400x300")
+app = Window(root)
+root.mainloop()
+
+# while True:
+		# time.sleep(4)
+		# command = input('Enter a number: ')
 		
-		if command == '1':
-			message=client.publish("IC.embedded/ALphawolfSquadron/send","get_data_light")
-			#print(mqtt.error_string(message.rc))
-			#print("I think I published a message")
-		if command == '2':
-			message=client.publish("IC.embedded/ALphawolfSquadron/send","get_data_sonic")
-			#print(mqtt.error_string(message.rc))
-			#print("I think I published a message")
-		if command == '3':
-			message=client.publish("IC.embedded/ALphawolfSquadron/send","get_empty")
-			#print(mqtt.error_string(message.rc))
-			#print("I think I published a message")
-		if command == '4':
-			message=client.publish("IC.embedded/ALphawolfSquadron/send","get_data")
-			#print(mqtt.error_string(message.rc))
-			#print("I think I published a message")
-		if command == 'q':
-			client.loop_stop()
-			exit()
-			
+		# if command == '1':
+			# message=client.publish("IC.embedded/ALphawolfSquadron/send","get_data_light")
+			# #print(mqtt.error_string(message.rc))
+			# #print("I think I published a message")
+		# if command == '2':
+			# message=client.publish("IC.embedded/ALphawolfSquadron/send","get_data_sonic")
+			# #print(mqtt.error_string(message.rc))
+			# #print("I think I published a message")
+		# if command == '3':
+			# message=client.publish("IC.embedded/ALphawolfSquadron/send","get_empty")
+			# #print(mqtt.error_string(message.rc))
+			# #print("I think I published a message")
+		# if command == '4':
+			# message=client.publish("IC.embedded/ALphawolfSquadron/send","get_data")
+			# #print(mqtt.error_string(message.rc))
+			# #print("I think I published a message")
+		# if command == 'q':
+			# client.loop_stop()
+			# exit()
+
